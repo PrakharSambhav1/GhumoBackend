2e9d63b4ff22b2f5a23d1754407c46ed116b4e84
root <root@DESKTOP-D5KUQAN.localdomain>
2020-08-23 14:34:42 +0530

    FIrst

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config/basic.env" });
+
+const app = express();
+
+app.listen();
